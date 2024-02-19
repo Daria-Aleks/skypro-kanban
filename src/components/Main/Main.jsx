ed63aee260ff5be330780b68a37d1e26178dcd00
AlexandraRad <94762892+AlexandraRad@users.noreply.github.com>
2024-02-19 14:07:54 +0300

    f

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,8 +2,9 @@ import Column from "../Column/Column"
 import { Container } from "../Common/Common.styled"
 import { MainBlock, MainContent, MainDiv } from "./Main.styled"
 import { BrowserRouter } from 'react-router-dom';
-
-
+import { useUser } from "../../hooks/useUser";
+// import { useUser } from "../../hooks/useUser";
+import Login from "../Login/Login";
 const statusList = [
     "Без статуса",
     "Нужно сделать",
@@ -13,23 +14,26 @@ const statusList = [
   ];
 
 function Main({cardList, isLoaded, error}){
+        // const {user} = useUser();
+
     return (
+
     <MainDiv>
-    <Container>
-        <MainBlock>
-            <MainContent>
-                {
-                    isLoaded? 'Loading' : error? "Не удалось загрузить данные, попробуйте позже ": statusList.map((item) => (
-                        <Column 
-                        key = {item}
-                        title = {item}
-                        cardList ={cardList?.filter((card => card.status === item))} />
-                    )) 
-                }
-            </MainContent>
-        </MainBlock>
-    </Container>
-    </MainDiv>
+        <Container>
+            <MainBlock>
+                <MainContent>
+                    {
+                        isLoaded? 'Loading' : error? "Не удалось загрузить данные, попробуйте позже ": statusList.map((item) => (
+                            <Column 
+                            key = {item}
+                            title = {item}
+                            cardList ={cardList?.filter((card => card.status === item))} />
+                        )) 
+                    }
+                </MainContent>
+            </MainBlock>
+        </Container>
+    </MainDiv> 
   
 )
 }
--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -2,10 +2,11 @@ import { Outlet, Navigate } from "react-router-dom";
 import { appRoutes } from "../../lib/appRoutes";
 import { useUser } from "../../hooks/useUser";
 import { useState } from "react";
-
+import Main from "../Main/Main";
 export default function PrivateRoute() {
   const {user} = useUser();
+  // сonsole.log(user)
   // const [user, setUser]= useState('user');
 
-  return user ? <Outlet/> : <Navigate to={appRoutes.MAIN}/>
+  return user ? <Outlet/> : <Navigate to={appRoutes.LOGIN}/>
 }
